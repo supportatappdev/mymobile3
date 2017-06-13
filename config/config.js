9d7419596fb3788ebf21f189f2693a98a65e1f75
Support <5upportatappdev@gmail.com>
2017-06-13 15:59:32 +0530

    Initial Checkin

---
+++ config/config.js
@@ -0,0 +1,40 @@
+/**
+ * mymobile3 
+ * Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    $urlRouterProvider.otherwise("/index/main");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "common/content.html",
+        })
+        .state('index.main', {
+            url: "/main",
+            templateUrl: "main.html",
+            data: { pageTitle: 'Example view' }
+        })
+        .state('index.minor', {
+            url: "/minor",
+            templateUrl: "minor.html",
+            data: { pageTitle: 'Example view' }
+        })
+}
+angular
+    .module('mymobile3')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+        $rootScope.appName = "mymobile3";
+    });
