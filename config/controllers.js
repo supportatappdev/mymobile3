ffaa2817d88680c751e920311f0f5831530f7bc2
Support <5upportatappdev@gmail.com>
2017-06-13 16:57:37 +0530

    Updated on Tue Jun 13 16:57:08 IST 2017

--- config/controllers.js
+++ config/controllers.js
@@ -3,15 +3,18 @@
 /**
  * MainCtrl - controller
  */
-function MainCtrl() {
-
-    this.userName = 'Example user';
-    this.helloText = 'Welcome in SeedProject';
-    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
-
-};
 
 
 angular
     .module('mymobile3')
-    .controller('MainCtrl', MainCtrl)
+    .controller('MainCtrl', function MainCtrl($scope,Cache,$location) {
+    if(!Cache.loggedInUser) {
+        $location.path("/login.html"); 
+        return;
+    }
+    this.userName = 'Example user';
+    this.helloText = 'Welcome in SeedProject';
+    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+   
+   
+})
